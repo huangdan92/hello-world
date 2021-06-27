a56a60271124cd72915378b7fc27d934645e18a7
huangdan05 <huangdan05@huangdan05deMacBook-Pro.local>
2021-06-27 21:04:52 +0800

    d

---
+++ mythreading.py
@@ -0,0 +1,15 @@
+import threading
+
+
+def thread_job():
+    print("this is an added thread ,nunber is %s" % threading.current_thread())
+
+def main():
+    added_thread =threading.Thread(target=thread_job())
+    added_thread.start()
+    # print((threading.active_count()))
+    # print(threading.enumerate())
+    # print(threading.current_thread())
+
+if __name__=='__main__':
+    main()
